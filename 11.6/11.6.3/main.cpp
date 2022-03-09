47813d039299264846a7c793ffa0808df889cadd
FBrut <anbondarchuk@yandex.ru>
2022-03-09 21:10:37 +1000

    Add

---
+++ 11.6/11.6.3/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+bool digit_IP(std::string str);
+bool control_IP(std::string str);
+bool digit(std::string str);
+
+
+bool control_IP(std::string str)
+{
+    if (str[0] == '.' || str[str.length() - 1] == '.') return false;
+	
+	for (int i = 0; i < str.length(); ++i)
+    {
+        if (str[i] == '.' && str[i + 1] == '.') return false;
+    }
+
+    if(!digit(str)) return false;
+
+    return true;
+}
+
+
+bool digit(std::string str)
+{
+    std::string s;
+    int i = 0;
+    s = str;
+
+    while (s[i])
+    {
+        if(s[i]=='.') {
+            s[i]=' ';
+        }
+        i++;
+    }
+
+
+    std::stringstream strm;
+    strm << s;
+    std::string a, b, c, d;
+    strm >> a >> b >> c >> d;
+
+    if(!digit_IP(a)) return false;
+	if(!digit_IP(b)) return false;
+	if(!digit_IP(c)) return false;
+	if(!digit_IP(d)) return false;
+
+    return true;
+}
+
+bool digit_IP(std::string str)
+{
+    if(str.size() > 1)
+    {
+	    if(str[0] == '0' && str[0 + 1] != '.') return false;
+    }
+
+	for (int i = 0; i < str.length(); i++)
+    {
+    	if(str[i] < '0' || str[i] > '9') return false;
+    }
+
+	int n = std::stoi(str);
+    if (n < 0 || n > 255) return false;
+
+    return true;
+}
+
+int main() {
+
+    std::string adrIP;
+
+    std::cout << "enter the IP address: " << std::endl;
+    std::getline(std::cin, adrIP);
+
+    std::cout << (control_IP(adrIP) ? "Yes" : "No") << std::endl;
+}
