32c04cfc79d2d893dde0c48d969c8ac64b737033
FBrut <83988753+FBrut@users.noreply.github.com>
2022-09-30 09:54:51 +1000

    Update main.cpp

--- 11.6/11.6.1/main.cpp
+++ 11.6/11.6.1/main.cpp
@@ -1,51 +1,49 @@
 //Шифр цезаря.
 
 #include <iostream>
-std::string encrypt_caesar(std::string, std::string, int);
-std::string decrypt_caesar(std::string, std::string, int);
+std::string encrypt_caesar(std::string, int);
+std::string decrypt_caesar(std::string, int);
 
-std::string encrypt_caesar(std::string text, std::string str, int key)
+std::string encrypt_caesar(std::string str, int key)
 {
+    key = (key % 26);
+    if (key < 0) {
+        for (int i = 0; i < str.length(); ++i) {
+            if (str[i] >= 'A' && str[i] <= 'Z') {
+                if (str[i + 1] == 'A') {
+                    str[i] = 'Z';
+                }
+                str[i] = str[i] + key;
+            } else if (str[i] >= 'a' && str[i] <= 'z') {
+                key = (key % 26);
+                str[i] = str[i] + key;
+            }
+            if(str[i] == ' ') {
+                str[i] = ' ';
+            }
+        }
+    }
     for (int i = 0; i < str.length(); ++i) {
-        bool isUpperChar{str[i] >= 'A' && str[i] <= 'Z'};
-        bool isLowerChar{str[i] >= 'a' && str[i] <= 'z'};
 
-        if (isUpperChar) {
-            if ((str[i] + key) > 'Z') {
-                str[i] += key - 'Z' + 'A' - 1;
-            } else if ((str[i] + key) < 'A') {
-                str[i] += key + 'Z' - 'A' + 1;
-            } else {
-                str[i] += key;
-            }
-            text += str[i];
-        } else if (isLowerChar) {
-            if ((str[i] + key) > 'z') {
-                str[i] += key - 'z' + 'a' - 1;
-            } else if ((str[i] + key) < 'a') {
-                str[i] += key + 'z' - 'a' + 1;
-            } else {
-                str[i] += key;
-            }
-            text += str[i];
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + key;
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] + key;
         }
         if(str[i] == ' ') {
             str[i] = ' ';
-            text += str[i];
         }
     }
-    return text;
+    return str;
 }
 
-std::string decrypt_caesar(std::string text, std::string str, int key)
+std::string decrypt_caesar(std::string str, int key)
 {
-    return encrypt_caesar(text, str, -key);
-
+    return encrypt_caesar(str, -key);
 }
 
 int main() {
     std::string str;
-    std::string text;
     int key;
 
     std::cout << "Enter the string:\n";
@@ -54,10 +52,10 @@ int main() {
     std::cout << "Enter key (number of letter shifts):\n";
     std::cin >> key;
 
-    str = encrypt_caesar(text, str, key);
+    str = encrypt_caesar(str, key);
     std::cout << str << std::endl;
 
     std::cout << "Decipher: \n";
-    str = decrypt_caesar(text, str,  key);
+    str = decrypt_caesar(str,  key);
     std::cout << str << std::endl;
 }
